b37909883f9a6b06d5c6876b87ea0a9c61155596
gtrixic <gtrixic@gmail.com>
2022-11-22 04:26:32 +0800

    updated tictactoe.h

--- tictactoe.h
+++ tictactoe.h
@@ -5,8 +5,8 @@ extern char player;
 extern char ai_player;
 bool empty_spots(char board_spots[3][3]);
 int check_win(char b[3][3]);
-int minimax(char board_spots[3][3], int depth, bool isMaxTurn);
+int minimax(char board_spots[3][3], int alpha, int beta, int depth, bool isMaxTurn);
 extern struct Move findNextBestMove(char board_spots[3][3]);
 
-int ez_minimax(char board_spots[3][3], int depth, bool isMaxTurn);
+int ez_minimax(char board_spots[3][3], int alpha, int beta, int depth, bool isMaxTurn);
 extern struct Move findNextMove(char board_spots[3][3]);
